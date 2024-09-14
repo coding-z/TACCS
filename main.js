4db50cc9b679d1bd73225a1e801e9b060b22e893
Zareef <zmas0005@student.monash.edu>
2024-09-14 22:05:49 +1000

    Fetch and display dependency version in browser window

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 const { app, BrowserWindow } = require("electron");
+const path = require("node:path");
 
+// Helper function to create browser window and load file
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js")
+    }
   });
 
   win.loadFile("index.html");
 };
 
+// Create the browser windows when the app is ready
 app.whenReady().then(() => {
   createWindow();
 });
