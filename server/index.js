44001074bedb86eca83e5f66b65541f33050840e
Zareef <zmas0005@student.monash.edu>
2024-09-21 11:45:46 +1000

    Add get endpoint

--- server/index.js
+++ server/index.js
@@ -3,6 +3,13 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const invoices = [];
+
+// Invoices endpoints
+app.get("/invoices", (req, res) => {
+  res.json(invoices);
+});
+
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
